079294ba785d8f0fd680bbd44f45d0cf86f63fed
Nikhil <nikhilganeshpatil@gmail.com>
2022-07-02 06:08:40 +0530

    I have added the css for the login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/login'
 import Profile from './pages/profile'
 import Home from './pages/home'
-
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/login' element={<Login></Login>} exact></Route>
-        <Route path='/profile' element={<Profile></Profile>} exact></Route>
-        <Route path='/' element={<Home></Home>} exact></Route>
+        <Route path='/login' element={<Login />} exact />
+        <Route path='/profile' element={<Profile />} exact />
+        <Route path='/' element={<Home />} exact />
       </Routes>
     </div>
   )
