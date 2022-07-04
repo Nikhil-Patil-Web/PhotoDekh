6f959f0e87c4dba294d67aedb030dfe72119ca54
Nikhil <nikhilganeshpatil@gmail.com>
2022-07-04 11:26:03 +0530

    I have added the function that makes an element disappear if you click outside

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,9 +1,17 @@
+import { useRef, useState } from 'react'
 import Header from '../../components/header'
+import useClickOutside from '../../helpers/clickOutside'
 
 export default function Home() {
+  const [visible, setVisible] = useState(true)
+  const el = useRef(null)
+  useClickOutside(el, () => {
+    setVisible(false)
+  })
   return (
     <div>
       <Header></Header>
+      {visible && <div className='card' ref={el}></div>}
     </div>
   )
 }
