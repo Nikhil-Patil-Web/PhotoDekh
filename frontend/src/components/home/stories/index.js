e419df603b90dab6897298374d81a7c8c16bc1dc
Nikhil <nikhilganeshpatil@gmail.com>
2022-07-06 16:16:54 +0530

    I have added the complete responsiveness to the home page

--- frontend/src/components/home/stories/index.js
+++ frontend/src/components/home/stories/index.js
@@ -2,7 +2,23 @@ import { ArrowRight, Plus } from '../../../svg'
 import { stories } from '../../../data/home'
 import './style.css'
 import Story from './Story'
+import { useMediaQuery } from 'react-responsive'
 export default function Stories() {
+  const query885px = useMediaQuery({ query: '(max-width: 885px)' })
+  const query960px = useMediaQuery({ query: '(max-width: 960px)' })
+  const query1030px = useMediaQuery({ query: '(max-width: 1030px)' })
+  const query1175px = useMediaQuery({
+    query: '(max-width:1175px)',
+  })
+  const max = query885px
+    ? 5
+    : query960px
+    ? 4
+    : query1030px
+    ? 5
+    : query1175px
+    ? 4
+    : stories.length
   return (
     <div className='stories'>
       <div className='create_story_card'>
@@ -16,7 +32,7 @@ export default function Stories() {
         </div>
         <div className='story_create_text'>Create Story</div>
       </div>
-      {stories.map((story, i) => (
+      {stories.slice(0, max).map((story, i) => (
         <Story story={story}></Story>
       ))}
       <div className='white_circle'>
