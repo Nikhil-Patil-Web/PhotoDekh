f4ca9e8a4b69b9f6816eb33fc9ff6858990d0048
Nikhil <nikhilganeshpatil@gmail.com>
2022-08-17 07:16:06 +0530

    I have created the entire backend to allow uploading of images to cloudinary.

--- backend/controllers/uploads.js
+++
@@ -1,7 +0,0 @@
-exports.uploadImages = async (req, res) => {
-  try {
-    res.json({ message: 'You are in the uploads controller' })
-  } catch (error) {
-    return res.status(500).json({ message: error?.message })
-  }
-}
--- backend/routes/uploads.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { uploadImages } = require('../controllers/uploads')
-const { authUser } = require('../middlewares/auth')
-
-router.post('/uploadImages', uploadImages)
-
-module.exports = router
