b2b31544ea6c5ffa72645881874be26f8769d71b
Nikhil <nikhilganeshpatil@gmail.com>
2022-09-17 11:45:02 +0530

    I have added the functionality for adding a post from the front to the back. Post creation with text is done.

--- frontend/src/components/createPostPopup/index.js
+++ frontend/src/components/createPostPopup/index.js
@@ -6,17 +6,38 @@ import EmojiPickerBackgrounds from './EmojiPickerBackgrounds'
 import AddToYourPost from './AddToYourPost'
 import ImagePreview from './ImagePreview'
 import useClickOutside from '../../helpers/clickOutside'
+import { createPost } from '../../functions/post'
+import PulseLoader from 'react-spinners/PulseLoader'
 
 export default function CreatePostPopup({ user, setVisible }) {
   const popup = useRef(null)
   const [text, setText] = useState('')
   const [userDetails, setUserDetails] = useState(true)
   const [showPrev, setShowPrev] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [images, setImages] = useState([])
   const [background, setBackground] = useState('')
   useClickOutside(popup, () => {
     setVisible(false)
   })
+
+  const postSubmit = async () => {
+    if (background) {
+      setLoading(true)
+      const res = await createPost(
+        null,
+        background,
+        text,
+        null,
+        user?.id,
+        user?.token
+      )
+      setLoading(false)
+      setBackground('')
+      setText('')
+      setVisible(false)
+    }
+  }
 
   if (userDetails) {
     return (
@@ -68,7 +89,19 @@ export default function CreatePostPopup({ user, setVisible }) {
             ></ImagePreview>
           )}
           <AddToYourPost setShowPrev={setShowPrev}></AddToYourPost>
-          <button className='post_submit'>Post</button>
+          <button
+            className='post_submit'
+            onClick={() => {
+              postSubmit()
+            }}
+            disabled={loading}
+          >
+            {loading ? (
+              <PulseLoader color='#fff' size={5}></PulseLoader>
+            ) : (
+              'Post'
+            )}
+          </button>
         </div>
       </div>
     )
